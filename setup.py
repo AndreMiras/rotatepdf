04e424715703ae0983ea0702b008cdec5ccec07c
Andre Miras <andre.miras@oridao.com>
2015-04-24 15:22:02 +0200

    simplifies distribution via setup script

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='rotatepdf',
+      version='20150425',
+      description='Rotate PDF easily from command line',
+      author='Andre Miras',
+      url='https://github.com/AndreMiras/rotatepdf',
+      packages=['rotatepdf'],
+      scripts=['rotatepdf/rotatepdf.py'],
+      install_requires=["PyPDF2"],)
